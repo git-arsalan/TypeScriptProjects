6f207f7b9e099bc3da8839925d448bf9a5dac815
Arsalan <arsalan.akkhter@gmail.com>
2023-09-16 13:52:28 +0500

    getting clone and executables on personal laptop

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,11 +1,10 @@
 #! /usr/bin/env node
-import chalk from 'chalk';
 import inquirer from 'inquirer';
 //import gradient from 'gradient-string';
 import chalkAnimation from 'chalk-animation';
 //import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
-console.log(chalk.bgGreen('Pakistan Zindabad!!!'));
+//console.log(chalk.bgGreen('Pakistan Zindabad!!!'));
 //let playerName;
 const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
 async function welcome() {
