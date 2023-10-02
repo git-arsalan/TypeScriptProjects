4370de3d3de14f32fe31d09bc1ed24d7e9610535
Arsalan <arsalan.akkhter@gmail.com>
2023-10-02 14:34:59 +0500

    completing currency conversion

---
+++ Curr_Conv/index.ts
@@ -0,0 +1,110 @@
+#! /usr/bin/env node
+
+import chalk from 'chalk';
+import inquirer from 'inquirer';
+import chalkAnimation from 'chalk-animation';
+//import figlet from 'figlet';
+import { createSpinner } from 'nanospinner';
+
+const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+
+ async function welcome() 
+ {
+  const rainbowTitle = chalkAnimation.rainbow('Welcome to Currency Exchange!\n');
+  await sleep();
+  rainbowTitle.stop();
+      
+}
+
+
+let obj_conversion = // creating currency converter object to handle currency rates
+{
+  "PKR":{
+    "USD": 0.0034,
+    "EUR": 0.0033,
+    "PKR": 1
+  },
+  "EUR":{
+    "USD": 1.06,
+    "EUR": 1,
+    "PKR": 306.60
+  },
+  "USD":{
+    "USD": 1,
+    "EUR": 0.95,
+    "PKR": 290
+  }
+
+}
+
+async function GetUserInputs()
+{
+    await sleep();
+    const userinput:{
+      from:"PKR"|"USD"|"EUR",
+      to:"PKR"|"USD"|"EUR",
+      amount:number
+    } = await inquirer.prompt([
+      {
+        name: 'from',
+        type: 'list',
+        message: 'Please select base currency:\n',
+        choices:["PKR","EUR","USD"]
+       
+      },
+      {
+        name: 'to',
+        type: 'list',
+        message: 'Please select conversion currency:\n',
+        choices:["PKR","EUR","USD"]
+       
+      },
+      {
+        name: 'amount',
+        type: 'number',
+        message: 'Please Enter conversion Amount:\n',
+        
+      }
+      
+    ]);
+
+    const {from, to, amount} = userinput;
+
+      return PerformConversion(userinput.from,userinput.to, amount);
+  }
+
+async function PerformConversion(fromcurr:"PKR"|"USD"|"EUR" , toCurr:"PKR"|"USD"|"EUR" , amount:number)
+{
+  try
+  {
+    const spinner = createSpinner('verifying your answer...').start();
+    if (fromcurr && toCurr && amount)
+    {
+        let res = obj_conversion[fromcurr][toCurr]  * amount;
+        spinner.success({ text: `your conversion from ${fromcurr} to ${toCurr} is : ${res}`});
+
+    }
+    else
+    {
+      spinner.error({text:`Invalid Input received!!!`})
+    }
+
+   
+  }
+  catch (Exception )
+  {
+    console.log("Exception occurred:");
+  }
+}
+
+
+
+
+
+
+
+ // Calling functions
+ console.clear();
+ welcome();
+ await sleep;
+ GetUserInputs();
