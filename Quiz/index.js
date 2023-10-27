bb4864a81ae87426881682ced41bf5a9e58c4dde
Arsalan <arsalan.akkhter@gmail.com>
2023-10-27 17:56:07 +0500

    completing quiz

--- Quiz/index.js
+++ Quiz/index.js
@@ -1,9 +1,44 @@
 #! /usr/bin/env node
+import inquirer from 'inquirer';
 import chalkAnimation from 'chalk-animation';
-const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+const sleep = (ms = 5000) => new Promise((r) => setTimeout(r, ms));
 async function welcome() {
     const rainbowTitle = chalkAnimation.rainbow('Welcome to General Knowledge Quiz!\n');
     await sleep();
     rainbowTitle.stop();
 }
-welcome();
+let apiUrl = "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple&encode=base64";
+// function for fetching quiz data from API
+let fetchData = async (dataUrl) => {
+    let Quiz = await fetch(dataUrl);
+    let res = await Quiz.json();
+    return res.results;
+};
+let apiRes = await fetchData(apiUrl);
+let startQuiz = async () => {
+    let score = 0;
+    const stud_Name = await inquirer.prompt({
+        name: 'name',
+        type: 'input',
+        message: 'Please Enter Student Name:\n'
+    });
+    for (let i = 0; i < 5; i++) {
+        let answers = [...apiRes[i].incorrect_answers, apiRes[i].correct_answer];
+        let quiz = await inquirer.prompt({
+            name: 'quest',
+            type: 'list',
+            message: atob(apiRes[i].question),
+            choices: answers.map((val) => atob(val))
+        });
+        if (quiz.quest === atob(apiRes[i].correct_answer)) {
+            score++;
+        }
+    }
+    console.log(score);
+};
+console.clear();
+await welcome();
+startQuiz();
+//console.log(apiRes);
+//startQuiz();
+//
--- Quiz/index.ts
+++ Quiz/index.ts
@@ -6,7 +6,7 @@ import chalkAnimation from 'chalk-animation';
 //import figlet from 'figlet';
 import { createSpinner } from 'nanospinner';
 
-const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
+const sleep = (ms = 5000) => new Promise((r) => setTimeout(r, ms));
 
  async function welcome() 
  {
@@ -16,6 +16,67 @@ const sleep = (ms = 2000) => new Promise((r) => setTimeout(r, ms));
       
 }
 
-welcome();
 
+
+let apiUrl = "https://opentdb.com/api.php?amount=5&category=18&difficulty=easy&type=multiple&encode=base64";
+
+
+// function for fetching quiz data from API
+
+
+
+let fetchData = async (dataUrl:string) => 
+{
+  let Quiz:any = await fetch(dataUrl);
+  let res = await Quiz.json();
+  return res.results;
+    
+}
+
+let apiRes = await fetchData(apiUrl);
+
+
+
+let startQuiz = async () => 
+{
+  let score = 0;
+  const stud_Name = await inquirer.prompt(
+        {
+          name: 'name',
+          type: 'input',
+          message: 'Please Enter Student Name:\n'
+        });
+    
+   for (let i = 0; i<5 ;i++)
+   {
+      let answers = [...apiRes[i].incorrect_answers, apiRes[i].correct_answer];
+
+      let quiz = await inquirer.prompt(
+        {
+          name: 'quest',
+          type: 'list',
+          message: atob(apiRes[i].question),
+          choices: answers.map((val:any)=> atob(val))
+        });
+
+        if (quiz.quest === atob(apiRes[i].correct_answer))
+        {
+          score++;
+        }
+   }
+
+   console.log(score);
+}
+
+
+console.clear();
+await welcome();
+startQuiz();
+//console.log(apiRes);
+//startQuiz();
+
+
+
+
+//
 
